2b3ef7e5b084cd18ad72a334ab530e2dce4fec8d
aialmohamed <ahmaedibrahim311@gmail.com>
2024-04-02 02:55:11 +0200

    Adde : Software Enviroment

---
+++ Software/app/src/main/java/software/Launcher.java
@@ -0,0 +1,11 @@
+package software;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        
+        Application.launch(MainApp.class,args);
+    }
+
+}
