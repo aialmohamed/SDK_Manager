80bae3b1e2227e760724563d3081cb7f641b8d64
Ahmed Ibrahim Almohamed <ahmaedibrahim311@gmail.com>
2024-04-14 01:47:57 +0200

    Remove : Firebase Repo

--- Software/app/src/main/java/software/login/viewmodel/LoginViewModel.java
+++ Software/app/src/main/java/software/login/viewmodel/LoginViewModel.java
@@ -1,13 +1,8 @@
 package software.login.viewmodel;
-
-import com.google.firebase.auth.ExportedUserRecord;
-import com.google.firebase.auth.ListUsersPage;
-
 import javafx.beans.property.ReadOnlyBooleanProperty;
 import javafx.beans.property.ReadOnlyBooleanWrapper;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
-import software.utils.FirebaseRepostory;
 
 public class LoginViewModel {
     private final StringProperty userEmail = new SimpleStringProperty();
@@ -15,10 +10,9 @@ public class LoginViewModel {
     private final ReadOnlyBooleanWrapper loginPosiable = new ReadOnlyBooleanWrapper();
     
     // Firebase connection
-    private FirebaseRepostory mFirebaseRepostory;
     public LoginViewModel() {
         loginPosiable.bind(userEmail.isNotEmpty().and(userPassword.isNotEmpty()));
-        mFirebaseRepostory = new FirebaseRepostory();
+
     }
 
     public StringProperty userEmailProperty()
